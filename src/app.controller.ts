c537787bb927bce57ebf69b173125e081f941f1a
5h4k4r <shakar.halabjai@yahoo.com>
2022-10-04 17:19:11 +0300

    Add logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @MessagePattern('greeting-async')
+  async getGreetingMessageAysnc(name: string): Promise<string> {
+    Logger.log(`Hello ${name} Async`);
+    return `Hello ${name} Async`;
   }
 }
